17c449b07c0d789446d2bd534444dc29df154d65
Hyeong-J <ignis8372@gmail.com>
2021-03-04 13:49:09 +0900

    feat: Get guess the answer

--- numguess/game.py
+++ numguess/game.py
@@ -5,4 +5,10 @@ user_name = input('Enter your name: ')
 answer = random.randint(1,100)
 
 #for debugging.
-print(user_name, answer)
+#print(user_name, answer)
+print(answer)
+
+guess = input('welcome {}. Please tell me the answer : '.format(user_name))
+
+# for debugging.
+print(answer,guess)
