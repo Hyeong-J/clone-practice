d1088bae041b21b5d9031fff1ab6fce3432de21b
Hyeong-J <ignis8372@gmail.com>
2021-03-04 11:11:21 +0900

    refactor : Set All String in Capital

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,5 @@
 for i in range(1,100+1):
-    if i %3 == 0 and i %5 == 0: print('fizzbuzz')
-    elif i %3 == 0 : print('fizz')
-    elif i %5 ==0 : print('buzz')
+    if i %3 == 0 and i %5 == 0: print('FIZZBUZZ')
+    elif i %3 == 0 : print('FIZZ')
+    elif i %5 ==0 : print('BUZZ')
     else: print(i)
