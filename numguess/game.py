2ddb84b30c3bc4c8c0683a9146c7e834ddeedbe5
Hyeong-J <ignis8372@gmail.com>
2021-03-04 13:53:12 +0900

    feat: Compare guess, answer and show result

--- numguess/game.py
+++ numguess/game.py
@@ -8,7 +8,12 @@ answer = random.randint(1,100)
 #print(user_name, answer)
 print(answer)
 
-guess = input('welcome {}. Please tell me the answer : '.format(user_name))
+guess = int(input('welcome {}. Please tell me the answer : '.format(user_name)))
 
 # for debugging.
 print(answer,guess)
+
+if guess==answer:
+    print('Correct!!')
+else:
+    print('Wrong!!! The answer was {}'.format(answer))
