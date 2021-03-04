77192edea38ac0bba4125b346f452de61597e2e5
Hyeong-J <ignis8372@gmail.com>
2021-03-04 11:41:03 +0900

    refactor : list comprehension

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,5 @@
-for i in range(1,100+1):
-    if i %3 == 0 and i %5 == 0: print('피즈버즈')
-    elif i %3 == 0 : print('FIZZ')
-    elif i %5 ==0 : print('BUZZ')
-    else: print(i)
+print(['fizzbuzz' if i%3 ==0 and i%5 ==0 
+    else 'fizz' if i%3==0 
+    else 'buzz' if i%5==0 
+    else i
+    for i in range(1,300+1)])
